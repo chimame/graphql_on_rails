09f729b984bca9436d63bb226280547da9b0e29e
chimame <rito.tamata@gmail.com>
2017-04-21 23:09:26 +0900

    rails g graphql:object Post title:String rating:Int comments:[Comment]

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,10 +4,20 @@ Types::QueryType = GraphQL::ObjectType.define do
   # They will be entry points for queries on your schema.
 
   # TODO: remove me
+=begin
   field :testField, types.String do
     description "An example field added by the generator"
     resolve ->(obj, args, ctx) {
       "Hello World!"
     }
+  end
+=end
+  description "The query root of this schema"
+
+  field :post do
+    type Types::PostType
+    argument :id, !types.ID
+    description "Find a Post by ID"
+    resolve ->(obj, args, ctx) { Post.find(args["id"]) }
   end
 end
